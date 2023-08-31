205130510cfa6df00d35a01dc8b14cc7bc86c53c
Victoria Isachenko <funn.willow@gmail.com>
2023-08-31 18:20:27 +0600

    Align with requirements.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketCost = 24376;
-        int milesCount = BonusMilesService.calculate(ticketCost);
-        System.out.printf("Стоимость билета: %s%n", ticketCost);
-        System.out.printf("Стоимость начислено миль: %s%n", milesCount);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
